d3e59f755bb70466699bbe4daf87a1487a2ac289
jipppat <Jirapat0716@gmail.com>
2024-01-27 11:53:43 +0700

    update

--- lib/main.dart
+++ lib/main.dart
@@ -11,115 +11,489 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Calculate',
       theme: ThemeData(
         // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+
+        colorScheme: ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 255, 159, 159)),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyCalculatorPage(title: 'MY CALCULATOR'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
+class MyCalculatorPage extends StatefulWidget {
+  const MyCalculatorPage({super.key, required this.title});
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<MyCalculatorPage> createState() => _MyCalculatorPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+class _MyCalculatorPageState extends State<MyCalculatorPage> {
+  String displayText = '';
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+        centerTitle: true,
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back), // ใช้ Icon ลูกศรตามต้องการ
+          onPressed: () {
+            // ตอบสนองการคลิกที่ Icon ลูกศร
+          },
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+      body: Column(
+        children: <Widget>[
+          buildAnswerWidget(),
+          buildNumPadWidget()
+        ],
+      ),
+    );    
+  }
+
+
+  Widget buildAnswerWidget() {
+    
+    return Container(
+        padding: EdgeInsets.all(16),
+        constraints: BoxConstraints.expand(height: 100),
+        color: Color.fromARGB(255, 255, 255, 255),
+        child: Align(
+            alignment: Alignment.bottomRight,
+            child: Column(mainAxisSize: MainAxisSize.min, children: <Widget>[
+              Text(displayText,
+                  style: TextStyle(fontSize: 48, fontWeight: FontWeight.bold))
+            ])));
+  }
+
+ Widget buildNumPadWidget() {
+  return Container(
+    color: Color.fromARGB(255, 255, 255, 255),
+    child: Column(
+      children: <Widget>[
+        SizedBox(height: 1), // ระยะห่างระหว่างคอลัมน์ด้านล่าง
+        Row(
+          children: <Widget>[
+            SizedBox(width: 16),
+            Expanded(
+              child: Container(
+                color: Color.fromARGB(255, 250, 157, 157),
+               height: 85,
+                child: Center(
+                  child: TextButton(
+                    onPressed: () {
+                      updateDisplayText('C');// โค้ดที่ต้องการให้ทำงานเมื่อคลิกปุ่มตัวเลข
+                    },
+                    child: Text(
+                      'C',
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(width: 8), 
+            Expanded(
+              child: Container(
+                color: Color.fromARGB(126, 207, 206, 220),
+                height: 85,
+                child: Center(
+                  child: IconButton(
+                    icon: Icon(Icons.backspace), 
+                    onPressed: () {
+                      setState(() {
+                          if (displayText.isNotEmpty) {
+                            displayText = displayText.substring(0, displayText.length - 1); // ลบตัวสุดท้าย
+                          }
+                        });
+                    },
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(width: 16),
+          ],
+        ),
+        SizedBox(height: 8), 
+        Row(
+          children: <Widget>[
+            SizedBox(width: 16),
+            Expanded(
+              child: Container(
+                color: Color.fromARGB(255, 250, 157, 157),
+               height: 85,
+                child: Center(
+                  child: TextButton(
+                    onPressed: () {
+                      updateDisplayText('7');
+                    },
+                    child: Text(
+                      '7',
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(width: 8),
+            Expanded(
+              child: Container(
+                color: Color.fromARGB(255, 250, 157, 157),
+               height: 85,
+                child: Center(
+                  child: TextButton(
+                    onPressed: () {
+                      updateDisplayText('8');
+                    },
+                    child: Text(
+                      '8',
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(width: 8),
+            Expanded(
+              child: Container(
+                color: Color.fromARGB(255, 250, 157, 157),
+               height: 85,
+                child: Center(
+                  child: TextButton(
+                    onPressed: () {
+                      setState(() {
+    
+                    });
+                      updateDisplayText('9');
+                    },
+                    child: Text(
+                      '9',
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(width: 8),
+            Expanded(
+              child: Container(
+                color: Color.fromARGB(126, 207, 206, 220),
+               height: 85,
+                child: Center(
+                  child: TextButton(
+                    onPressed: () {
+                      
+                    },
+                    child: Text(
+                      '÷',
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(width: 16),
+          ],
+        ),
+        SizedBox(height: 8), 
+        Row(
+          children: <Widget>[
+            SizedBox(width: 16),
+            Expanded(
+              child: Container(
+                color: Color.fromARGB(255, 250, 157, 157),
+               height: 85,
+                child: Center(
+                  child: TextButton(
+                    onPressed: () {
+                      updateDisplayText('4');// โค้ดที่ต้องการให้ทำงานเมื่อคลิกปุ่มตัวเลข
+                    },
+                    child: Text(
+                      '4',
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(width: 8),
+            Expanded(
+              child: Container(
+                color: Color.fromARGB(255, 250, 157, 157),
+               height: 85,
+                child: Center(
+                  child: TextButton(
+                    onPressed: () {
+                      updateDisplayText('5');// โค้ดที่ต้องการให้ทำงานเมื่อคลิกปุ่มตัวเลข
+                    },
+                    child: Text(
+                      '5',
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(width: 8),
+            Expanded(
+              child: Container(
+                color: Color.fromARGB(255, 250, 157, 157),
+               height: 85,
+                child: Center(
+                  child: TextButton(
+                    onPressed: () {
+                      updateDisplayText('6');// โค้ดที่ต้องการให้ทำงานเมื่อคลิกปุ่มตัวเลข
+                    },
+                    child: Text(
+                      '6',
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(width: 8),
+            Expanded(
+              child: Container(
+                color: Color.fromARGB(126, 207, 206, 220),
+               height: 85,
+                child: Center(
+                  child: TextButton(
+                    onPressed: () {
+                      // โค้ดที่ต้องการให้ทำงานเมื่อคลิกปุ่มตัวเลข
+                    },
+                    child: Text(
+                      'x',
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(width: 16),
+          ],
+        ),
+        SizedBox(height: 8), // ระยะห่างระหว่างคอลัมน์ด้านล่าง
+        Row(
+          children: <Widget>[
+            SizedBox(width: 16),
+            Expanded(
+              child: Container(
+                color: Color.fromARGB(255, 250, 157, 157),
+               height: 85,
+                child: Center(
+                  child: TextButton(
+                    onPressed: () {
+                      updateDisplayText('1');// โค้ดที่ต้องการให้ทำงานเมื่อคลิกปุ่มตัวเลข
+                    },
+                    child: Text(
+                      '1',
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(width: 8),
+            Expanded(
+              child: Container(
+                color: Color.fromARGB(255, 250, 157, 157),
+               height: 85,
+                child: Center(
+                  child: TextButton(
+                    onPressed: () {
+                      updateDisplayText('2');// โค้ดที่ต้องการให้ทำงานเมื่อคลิกปุ่มตัวเลข
+                    },
+                    child: Text(
+                      '2',
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(width: 8),
+            Expanded(
+              child: Container(
+                color: Color.fromARGB(255, 250, 157, 157),
+                height: 85,
+                child: Center(
+                  child: TextButton(
+                    onPressed: () {
+                      updateDisplayText('3');// โค้ดที่ต้องการให้ทำงานเมื่อคลิกปุ่มตัวเลข
+                    },
+                    child: Text(
+                      '3',
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(width: 8),
+            Expanded(
+              child: Container(
+                color: Color.fromARGB(126, 207, 206, 220),
+                 height: 85,
+                child: Center(
+                  child: TextButton(
+                    onPressed: () {
+                      // โค้ดที่ต้องการให้ทำงานเมื่อคลิกปุ่มตัวเลข
+                    },
+                    child: Text(
+                      '-',
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(width: 16),
+          ],
+        ),
+        SizedBox(height: 8),
+             Row(
+          children: <Widget>[
+            SizedBox(width: 16),
+            Expanded(
+              flex: 3,
+              child: Container(
+                color: Color.fromARGB(255, 250, 157, 157),
+               height: 85,
+                child: Center(
+                  child: TextButton(
+                    onPressed: () {
+                      updateDisplayText('0');// โค้ดที่ต้องการให้ทำงานเมื่อคลิกปุ่มตัวเลข
+                    },
+                    child: Text(
+                      '0',
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(width: 8),
+            Expanded(
+              
+              child: Container(
+                color: Color.fromARGB(126, 207, 206, 220),
+               height: 85,
+                child: Center(
+                  child: TextButton(
+                    onPressed: () {
+                      // โค้ดที่ต้องการให้ทำงานเมื่อคลิกปุ่มตัวเลข
+                    },
+                    child: Text(
+                      '+',
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            
+           
+            
+            SizedBox(width: 16),
+            
+          ],
+        ),
+        SizedBox(height: 8), // ระยะห่างระหว่างคอลัมน์ด้านล่าง
+        Row(
+          children: <Widget>[
+            SizedBox(width: 16),
+            Expanded(
+              child: Container(
+                color: Color.fromARGB(255, 255, 99, 99),
+               height: 85,
+                child: Center(
+                  child: TextButton(
+                    onPressed: () {
+                      displayText = '';// โค้ดที่ต้องการให้ทำงานเมื่อคลิกปุ่มตัวเลข
+                    },
+                    child: Text(
+                      '=',
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            
+            
+            SizedBox(width: 16),
+          ],
+        ),
+      ],
+    ),
+  );
+}
+
+void updateDisplayText(String newText) {
+    setState(() {
+     if (newText == 'C'|| newText == '=') {
+        displayText = ''; // ลบทุกอย่าง
+      } else {
+        displayText += newText; // เพิ่มค่าใหม่ไปยัง displayText
+      }// เพิ่มตัวเลขที่ถูกคลิกลงใน displayText
+    });
   }
 }
+
+
+
+
